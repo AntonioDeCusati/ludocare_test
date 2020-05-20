38b10fd5866b14431a86ac97ccd026457242ea2f
AntonioDeCusati <a.decusati@nesea.it>
2020-05-20 17:47:10 +0200

    Al momento stabile ma disattivati i salvataggi per recuperare i dati e mestrarli nella dashboard per poi aggiungere le azioni di ingresso e di altri dati

--- src/assets/reko/script.js
+++ src/assets/reko/script.js
@@ -7,16 +7,9 @@ Promise.all([
   faceapi.nets.ssdMobilenetv1.loadFromUri('./assets/reko/models')
   // faceLandmark68TinyNet
   // tinyYolov2
-]).then(()=>{
-  console.log(dbUsed)
-  allFaces = db.collection(dbUsed).get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      var data = doc.data()
-      data.descriptor = Float32Array.from(doc.data().descriptor)
-      var labeled = new faceapi.LabeledFaceDescriptors(data.id, [data.descriptor] )
-      faceFromDB.push(labeled)
-    });
-  });
+]).then(() => {
+  console.log("Db used for getting data:", dbUsed);
+  refreshData();
   startVideo()
 })
 
@@ -52,6 +45,23 @@ function startVideo() {
 
 }
 
+function refreshData() {
+
+  db.collection(dbUsed).get().then(
+    (res) => {
+      faceFromDB = [];
+      res.forEach(doc => {
+        var data = doc.data()
+        data.descriptor = Float32Array.from(doc.data().descriptor)
+        var labeled = new faceapi.LabeledFaceDescriptors(data.id, [data.descriptor])
+        faceFromDB.push(labeled)
+      });
+
+      console.log(faceFromDB);
+    }
+  );
+}
+
 video.addEventListener('play', () => {
   var creatoDeku = false;
   if (document.getElementById("canvasClr") != undefined) {
@@ -66,46 +76,89 @@ video.addEventListener('play', () => {
   faceapi.matchDimensions(canvas, displaySize)
 
   setInterval(async () => {
+    refreshData();
+
     var detections = await faceapi.detectAllFaces(video, option).withFaceLandmarks().withFaceExpressions().withAgeAndGender().withFaceDescriptors()
-    if (detections && detections.length > 0) {
+
+    if (!detections.length) {
+      return
+    } else {
       detectionsFace.innerHTML = "";
       detectionsFace.value = JSON.stringify(detections);
     }
-    if (!detections.length) {
-      return
-    }
+
     let faces = detections;
-    var faceMatcher = new faceapi.FaceMatcher(faceFromDB, 0.6)
-
     var resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-
-
-    var bestMatch = faceMatcher.findBestMatch(faces[0].descriptor)
-    if(bestMatch.label === "unknown"){
-      console.info("Persona sconosciuta, la sto salvando tutta la faccia: ", faces[0])
-      db.collection('test').add({
-        id:"temp"
-      }).then(ref => {
-        console.log('Added document with ID: ', ref.id);
-        let faceRef = db.collection('test').doc(ref.id);
-        faceRef.update({id: ref.id, label: ref.id});
-      });
-
-    }
-
-
     var box = resizedDetections[0].detection.box
-    var drawBox = new faceapi.draw.DrawBox(box, { label: bestMatch.label })
-    drawBox.draw(canvas)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+
+    if (faceFromDB.length > 0) {
+      var faceMatcher = new faceapi.FaceMatcher(faceFromDB, 0.6);
+      var bestMatch = faceMatcher.findBestMatch(faces[0].descriptor);
+      var drawBox = new faceapi.draw.DrawBox(box, { label: bestMatch.label })
+
+      if (bestMatch.label === "unknown") {
+        //Faccio saltare la prima volta per non creare troppi volti qualora esistesse gia
+        if (volte < 3) { //futuro 8
+          console.info("Volto non riconosciuto ma non salvato, al prossimo giro lo salvo");
+          volte += 1;
+        } else {
+          volte = 0;
+          console.info("Persona sconosciuta, sto salvando tutta la faccia: ", faces[0])
+          console.log("Score: ", resizedDetections[0].detection.score)
+
+          let tempFace = resizedDetections[0];
+          if (resizedDetections[0].detection.score > 0.96) {
+           /* db.collection(dbUsed).add(
+              {
+                id: "temp",
+                age: tempFace.age,
+                descriptor: Array.from(tempFace.descriptor),
+                score: tempFace.detection.score,
+                expressions: JSON.parse(JSON.stringify(tempFace.expressions)),
+                gender: tempFace.gender,
+                genderProbability: tempFace.genderProbability
+              }
+            ).then(ref => {
+              console.log('Added document with ID: ', ref.id);
+              let faceRef = db.collection(dbUsed).doc(ref.id);
+              faceRef.update({ id: ref.id, label: ref.id });
+            });*/
+          }
+        }
+      } else {
+        // il viso è stato riconosciuto
 
 
-    
-    if (faces.length == 1) {
-      this.currentFaceDescriptor = faces[0].descriptor;
+
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        drawBox.draw(canvas)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+      }
+
+    } else {
+      //salvo la prima faccia sul db
+      console.log(faceFromDB);
+
+      let tempFace = resizedDetections[0];
+      if (tempFace.detection.score > 0.90) {
+       /* db.collection(dbUsed).add(*
+          {
+            id: "temp",
+            age: tempFace.age,
+            descriptor: Array.from(tempFace.descriptor),
+            score: tempFace.detection.score,
+            expressions: JSON.parse(JSON.stringify(tempFace.expressions)),
+            gender: tempFace.gender,
+            genderProbability: tempFace.genderProbability
+          }
+        ).then(ref => {
+          console.log('Added document with ID: ', ref.id);
+          let faceRef = db.collection(dbUsed).doc(ref.id);
+          faceRef.update({ id: ref.id, label: ref.id });
+        });*/
+      }
     }
 
 
-  }, 1500)
+  }, 1500) //metterò 250 forse
 })
